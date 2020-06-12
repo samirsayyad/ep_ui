80f1e36dfb52d9580110e8c930a7be36412face2
Samir Sayyad <samir.saiad@gmail.com>
2020-06-12 14:36:19 +0430

    add dropshadow of pad and change font

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,4 @@ exports.documentReady = function(){
     $('#pad_title').prepend('<a href="../../"><img class="logo" src="../static/images/logo.png"> </a>');
    
 }
+exports.aceEditorCSS = function(hook_name, cb){return ["/ep_ui/static/css/main.css"];} // inner pad CSS
